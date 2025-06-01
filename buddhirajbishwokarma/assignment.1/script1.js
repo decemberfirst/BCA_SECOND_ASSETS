18e82d36ee3b5444cc3e01a7bc0844c3fe6de86a
buddhi raj bishwokarma <buddhirajbishwokarma387@gmail.com>
2025-06-02 03:17:26 +0545

    buddhi chachha

---
+++ buddhirajbishwokarma/assignment.1/script1.js
@@ -0,0 +1,22 @@
+let nestedObject ={
+    address:{
+        from:{
+            permanent : "Nepal",
+            temporary : " India"
+        },
+    },
+};
+
+let think = Object.entries(nestedObject.address.from);
+for(let i = 0; i<think.length; i++){
+    let [key,value] = think[i];
+   console.log(`The Key IS :${key} and Valu IS : ${value}`)
+};
+
+
+
+let think1 = Object.values(nestedObject.address.from);
+for(let i = 0; i<think1.length; i++){
+    let value = think1[i];
+   console.log(`The Valu IS : ${value}`);
+}
