1827a0b6a1448a8a3eac6c51537c4820f37869f9
Samikshya <samikshyabhusal066.com>
2025-06-01 13:12:26 +0545

    assigment

---
+++ Samikshya_Bhusal/assignment-1/script.js
@@ -0,0 +1,9 @@
+//object
+let myAnotherObject={
+    name:'sami',
+    address:'nepal',
+    rollno:13,
+}
+console.log(myAnotherObject);
+myAnotherObject.name='sann';//update property name->sann
+console.log(myAnotherObject);
