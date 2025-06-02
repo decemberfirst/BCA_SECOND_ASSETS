cd7cbe1a3e82b07e6bc7fbdda29cd7880d1e2f63
Pujan-Pandey <your-email@example.com>
2025-06-02 12:51:00 +0545

    Code Added

---
+++ pujan-pandey/Assignments/Assignment1/script.js
@@ -0,0 +1,15 @@
+//qn
+let personal={
+    fName:'ram',
+    lName:'poudel',
+    contact:9800,
+    address:'Nepal',
+    printName(){
+        console.log('Name:',this.fName);
+    },
+  printAddress(){
+console.log('Address:',this.address)
+}
+}
+personal.printAddress();
+personal.printName();
