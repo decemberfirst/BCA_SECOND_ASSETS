2d8579a7db4e15127186a0232d9e5e33d94e50e5
Your Name <you@example.com>
2025-06-02 22:14:16 +0545

    Day-1

---
+++ ANISHA-CHHANTEL/PRACTICE/Day-1/script.js
@@ -0,0 +1,24 @@
+let firstNumber = 10;
+let secondNumber = 30;
+let thirdNumber = 40;
+
+console.log(firstNumber + secondNumber);
+console.log(firstNumber - secondNumber);
+
+// comments
+
+// comment doesnot count as code, this is ignored by the browser
+
+// single line comment, multi line comment
+
+/**
+ * this is multi line comment
+ * this is another line
+ * this is also another line
+ */
+
+const myAnotherNumber = 20;
+// myAnotherNumber = 30; this throws an error, constants cannot be changed
+
+let myLastNumber = 40;
+console.log(myLastNumber);
