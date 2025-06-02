c2a2d54002b9e70fc1f68cec340ae1a3fc6d3964
mohit12-dev <mohitsapkota678@gmail.com>
2025-06-02 12:40:38 +0545

    Destructuring

---
+++ Mohit-Sapkota/assignment/Ass1/scrip.js
@@ -0,0 +1,14 @@
+let nestedObject = {
+  address: {
+    from: {
+      permanent: 'nepal',
+      temporary: 'india', // TODO: now destructure permanent and temporary
+    },
+  },
+};
+
+let myObject=Object.entries(nestedObject.address.from)
+for(let i=0;i<myObject.length;i++){
+  let [keys,values]=myObject[i];
+  console.log(`${keys} : ${values}`);
+}
