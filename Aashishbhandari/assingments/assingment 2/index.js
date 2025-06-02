01c1b11d2dbc9c0b6d6036b8fb3c9189ed356b11
AashishBhandari <you@example.com>
2025-06-02 22:21:12 +0545

    aashish-bhandari

--- Aashishbhandari/assingments/assingment 2/index.js
+++ Aashishbhandari/assingments/assingment 2/index.js
@@ -9,5 +9,5 @@ let nestedObject = {
 let entries = Object.entries(nestedObject.address.from);
 for(let m = 0; m<entries.length;m++){
     let[keys,values] = entries[m];
-      console.log(${keys} : ${values});
+      console.log(`${keys} : ${values}`);
     };
