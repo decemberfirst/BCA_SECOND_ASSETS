a4e4650a30416787e311211d430057d3b8dafc4e
Agraj Adhikari <agrajadhikari7890@gmail.com>
2025-06-02 15:33:35 +0545

    Assignment3

---
+++ Agraj Adhikari/Assignment/Assignment 3/script.js
@@ -0,0 +1,55 @@
+let {animalType , animalName:{scientific:{name : scientificname}} , animalName:{callable:{name : callablename}}} = {
+    animalType: "Amphibians",
+    animalHabitat: "Water and land",
+    animalName: {
+        scientific: {
+            name: "Anura",
+        },
+        callable: {
+            name : 'Frog',
+        },
+    },
+};
+
+console.log(animalType, scientificname, callablename);
+
+ 
+//(print StudentRole,Studentfirstname,first Subject name and first Subject address name)
+
+let {StudentRole, 
+
+    StudentDetails:{StudentPersonalDetails:{firstname}},
+
+    StudentDetails:{StudentFamilyDetails:{SubjectDetails:{firstSubject:{name:firstSubjectname}}}},
+
+    StudentDetails:{StudentFamilyDetails:{SubjectDetails:{firstSubject:{adress:{name:addressname}}}}},
+
+    } = {
+    StudentId: "A100",
+    StudentRole: 'ADMIN',
+    StudentDetails: {
+        StudentPersonalDetails:{
+            firstname: 'Agraj',
+            lastname: 'Adhikari',
+            from: 'Butwal',
+            isStudent: true,
+        },
+        StudentFamilyDetails:{
+            SubjectDetails: {
+                hasSubject: true,
+                numberofSubject: 2,
+                firstSubject: {
+                    name:'Maths',
+                    adress:{
+                        name:'Butwal',
+                    },
+                },
+            },
+        },
+    },
+};
+
+console.log(StudentRole,firstname,firstSubjectname,addressname);
+
+
+
