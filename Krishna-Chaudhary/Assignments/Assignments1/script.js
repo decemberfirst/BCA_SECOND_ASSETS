3d38ee747eeaa400dc332e0b3ec294c9ad59b7ff
Bijay Sunar <bijaysunar506@gmail.com>
2025-06-03 20:36:08 +0545

    Code Not Uploaded

--- Krishna-Chaudhary/Assignments/Assignments1/script.js
+++ Krishna-Chaudhary/Assignments/Assignments1/script.js
@@ -1,16 +1,16 @@
-let nestedObject = {
-    address : {
-        from : {
-            permanent : "nepal",
-            temporary : "india"
-        },
-    },
-};
-let entries = Object.entries(nestedObject.address.from);
-for(let m = 0; m<entries.length;m++){
-    let[keys,values] = entries[m];
-      console.log(`${keys} : ${values}`);
+// let nestedObject = {
+//     address : {
+//         from : {
+//             permanent : "nepal",
+//             temporary : "india"
+//         },
+//     },
+// };
+// let entries = Object.entries(nestedObject.address.from);
+// for(let m = 0; m<entries.length;m++){
+//     let[keys,values] = entries[m];
+//       console.log(`${keys} : ${values}`);
     
 
-};
+// };
 
